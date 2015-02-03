70bb77f4debf3eabdf231db4204282ad9746414f
Jared Combs <jared.combs@tstllc.net>
2015-02-02 23:54:11 -0500

    fixed unnecessary config require

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,22 +1,20 @@
 'use strict';
 
-require(['require-config'], function() {
-    require(['jquery', 'bootstrap'], function () {
-        $(function () {
-            $('a[href^="#"]').on('click', function(event) {
+require(['jquery', 'bootstrap'], function () {
+    $(function () {
+        $('a[href^="#"]').on('click', function(event) {
 
-                var target = $( $(this).attr('href') );
+            var target = $( $(this).attr('href') );
 
-                console.log(target.length);
+            console.log(target.length);
 
-                if( target.length ) {
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 800);
-                }
+            if( target.length ) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 800);
+            }
 
-            });
         });
     });
 });
